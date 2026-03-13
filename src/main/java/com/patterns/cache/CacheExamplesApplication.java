a0c6296b8fc58dfa3d5fc8e2068b0ab39c57ba86
JoaoDallagnol <jvdallagnol2001@gmail.com>
2026-03-13 10:47:56 -0300

    Initialing cache-patterns project

---
+++ src/main/java/com/patterns/cache/CacheExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.patterns.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheExamplesApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CacheExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patterns/cache/CacheExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patterns.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
